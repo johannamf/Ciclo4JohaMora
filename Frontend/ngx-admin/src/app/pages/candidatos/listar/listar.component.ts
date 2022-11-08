90b849f535bff2affc1181b528ed691537b6b617
Johanna Mora Fajardo <johanafajardo13@gmail.com>
2022-11-07 23:16:22 -0500

    Guia 5.8 completa.

--- Frontend/ngx-admin/src/app/pages/candidatos/listar/listar.component.ts
+++ Frontend/ngx-admin/src/app/pages/candidatos/listar/listar.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import Swal from 'sweetalert2';
+import {
+  Candidato
+} from '../../../modelos/candidato.model';
+import {
+  CandidatosService
+} from '../../../servicios/candidato.service';
 @Component({
   selector: 'ngx-listar',
   templateUrl: './listar.component.html',
   styleUrls: ['./listar.component.scss']
 })
 export class ListarComponent implements OnInit {
-
-  constructor() { }
-
+  candidatos: Candidato[];
+  nombresColumnas: string[] = ['Cedula', 'Nombre', 'Apellido', 'Opciones'];
+  constructor(private miServicioCandidatos: CandidatosService) {}
   ngOnInit(): void {
+      this.listar();
   }
-
+  listar(): void {
+      this.miServicioCandidatos.listar().
+      subscribe(data => {
+          this.candidatos = data;
+      });
+  }
+  agregar(): void {
+      console.log("agregando nuevo")
+  }
+  editar(id: string): void {
+      console.log("editando a " + id)
+  }
+  eliminar(id: string): void {
+      Swal.fire({
+          title: 'Eliminar Candidato',
+          text: "Está seguro que quiere eliminar el candidato?",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si, eliminar'
+      }).then((result) => {
+          if (result.isConfirmed) {
+              this.miServicioCandidatos.eliminar(id).
+              subscribe(data => {
+                  Swal.fire(
+                      'Eliminado!',
+                      'El candidato ha sido eliminada correctamente',
+                      'success'
+                  )
+                  this.ngOnInit();
+              });
+          }
+      })
+  }
 }
